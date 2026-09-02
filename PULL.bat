e62a9a17823fb41a5286996881e982a28df531e8
Zbramus NT <Zbramus_NT>
2026-09-02 18:05:19 +0200

    Session du 02/09/2026 a 18:05:19,02

---
+++ PULL.bat
@@ -0,0 +1,14 @@
+@echo off
+echo === Recuperation des derniers changements ===
+
+git pull
+
+echo === Nettoyage des fichiers temporaires Ren'Py ===
+
+for /r %%f in (*.rpyc) do del "%%f"
+for /r %%f in (*.rpymc) do del "%%f"
+
+if exist "game\cache" rd /s /q "game\cache"
+
+echo === Termine ===
+pause
